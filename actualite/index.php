f265982f5758e715924a7c66dfa32888eb67d192
sullivan-treguier-dev <sullivan.treguier.lycee@gmail.com>
2025-12-16 14:19:22 +0100

    Add files via upload

--- actualite/index.php
+++ actualite/index.php
@@ -3,24 +3,13 @@ $title = "Accueil";
 $selected_accueil = true;
 require_once 'pdo/pdo.php';
 require_once 'layout/layout_debut.php';
-require_once 'posts/Post.php';
+require_once 'class/Createur.php';
+require_once 'class/Post.php';
 ?>
 <div>
     <?php
     if (!isset($e)) {
-        $sql = "SELECT * FROM posts";
-        $temp = $pdo->query($sql);
-        while ($resultat = $temp->fetch()) {
-            $post = new Post($resultat['titre'], $resultat['contenu']);
-            echo "<form action='article_details.php' method='get'>";
-            echo "<div>";
-            echo "<h1>" . $post->titre . "</h1>";
-            echo "<p>" . $post->contenu . "</p>";
-            echo "<input type='hidden' name='{$resultat['id']}'>";
-            echo "<button type='submit'}'>Voir les détails</button>";
-            echo "</div>";
-            echo "</form>";
-        }
+        $post->afficheCinqArticle("SELECT * FROM posts");
     }
     ?>
 </div>
