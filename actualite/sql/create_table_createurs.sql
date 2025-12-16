d1af34750dbcd04da50d524862467ed4197bde85
sullivan-treguier-dev <sullivan.treguier.lycee@gmail.com>
2025-12-16 14:40:00 +0100

    Add files via upload

---
+++ actualite/sql/create_table_createurs.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS createurs (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nom VARCHAR(255) NOT NULL,
+    prenom VARCHAR(255) NOT NULL,
+    linkedin VARCHAR(255) NOT NULL,
+    mail VARCHAR(255) NOT NULL,
+    telephone VARCHAR(255) NOT NULL,
+);
